ca3643c14f69c3da403d2859c2dc31c47cf365cf
jasjotp <jasjotp@sfu.ca>
2025-08-12 23:51:47 -0700

    added langchain-tavily to requirements.txt

--- tools/tools.py
+++ tools/tools.py
@@ -1,9 +1,9 @@
-from langchain_tavily import TavilySearchResults
+from langchain_tavily import TavilySearch
 
 def get_profile_url_tavily(name: str):
     '''
     function that takes a name as input and finds its LinkedIn or Twitter URL from Tavily 
     '''
-    search = TavilySearchResults()
+    search = TavilySearch()
     res = search.run(f"{name}")
     return res
